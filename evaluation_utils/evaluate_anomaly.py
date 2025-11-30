ef85cdb59ef24291bc2c3e8fba924b421fe71013
Haochen Zhang <zhc@vpn-172-27-7-176.host.ucla.edu>
2025-11-30 14:03:32 +0800

    flow evaluate ready

--- evaluation_utils/evaluate_anomaly.py
+++ evaluation_utils/evaluate_anomaly.py
@@ -4,45 +4,57 @@ import numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score, f1_score
 from tqdm import tqdm
 
-def fit_classifier(model, train_loader, test_loader, lr):
+
+def infinite_loader(dataloader):
+    while True:
+        for batch in dataloader:
+            yield batch
+
+def fit_classifier(model, normal_train_loader, anomaly_train_loader, test_loader, lr):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     best_eval = float('inf')
     no_improvement = 0
+    train_loss = 0
+    train_seen = 0
 
-    for epoch in range(100):
-        model.train()
-        train_loss = 0
-        train_seen = 0
-        for inputs, labels in tqdm(train_loader, desc=f"Training Classifier: Epoch{epoch}"):
-            optimizer.zero_grad()
-            loss = model.loss(inputs, labels)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-            optimizer.step()
-            train_loss += loss.item()
-            train_seen += 1
+    model.train()
+    for iteration in tqdm(range(10000)):
+        normal_inputs, normal_labels = next(normal_train_loader)
+        anomaly_inputs, anomaly_labels = next(anomaly_train_loader)
+        inputs = torch.cat((normal_inputs, anomaly_inputs), dim=0)
+        labels = torch.cat((normal_labels, anomaly_labels), dim=0)
+        loss = model.loss(inputs, labels)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+        train_loss += loss.item()
+        train_seen += 1
+        if iteration % 250 == 0 and iteration > 0:
+            train_loss_avg = train_loss / train_seen
+            model.eval()
+            eval_loss = 0
+            eval_seen = 0
 
-        train_loss_avg = train_loss / train_seen
-        model.eval()
-        eval_loss = 0
-        eval_seen = 0
+            for inputs, labels in test_loader:
+                with torch.no_grad():
+                    loss = model.loss(inputs, labels)
+                bs = labels.size(0)
+                eval_loss += loss.item() * bs
+                eval_seen += bs
 
-        for inputs, labels in test_loader:
-            with torch.no_grad():
-                loss = model.loss(inputs, labels)
-            bs = labels.size(0)
-            eval_loss += loss.item() * bs
-            eval_seen += bs
+            eval_loss = eval_loss / eval_seen
+            print(f"Step{iteration} | train loss: {train_loss_avg:.4f} | eval loss: {eval_loss:.4f} ||")
+            if eval_loss < best_eval:
+                best_eval = eval_loss
+                no_improvement = 0
+            else:
+                no_improvement += 1
+            if no_improvement > 10:
+                break
 
-        eval_loss = eval_loss / eval_seen
-        print(f"Epoch{epoch} | train loss: {train_loss_avg:.4f} | eval loss: {eval_loss:.4f} ||")
-        if eval_loss < best_eval:
-            best_eval = eval_loss
-            no_improvement = 0
-        else:
-            no_improvement += 1
-        if no_improvement > 10:
-            break
+            train_loss = 0
+            train_seen = 0
+            model.train()
 
     # ------------------ Collect Predictions ------------------
     all_preds = []
@@ -103,10 +115,15 @@ def run_anomaly_quality_test(
     else:
         raise ValueError("mode must be interval or timestep")
 
-    train_set_input = torch.cat([normal, anomaly], dim=0)
-    train_set_label = torch.cat([normal_label, anomaly_label], dim=0)
-    train_set = TensorDataset(train_set_input, train_set_label)
-    train_loader = DataLoader(train_set, batch_size=bs, shuffle=True)
+    # train_set_input = torch.cat([normal, anomaly], dim=0)
+    # train_set_label = torch.cat([normal_label, anomaly_label], dim=0)
+    normal_train_set = TensorDataset(normal, normal_label)
+    anomaly_train_set = TensorDataset(anomaly, anomaly_label)
+
+    normal_train_loader = DataLoader(normal_train_set, batch_size=bs, shuffle=True, drop_last=True)
+    anomaly_train_loader = DataLoader(anomaly_train_set, batch_size=bs, shuffle=True, drop_last=True)
+    normal_train_loader = infinite_loader(normal_train_loader)
+    anomaly_train_loader = infinite_loader(anomaly_train_loader)
 
     '''test set'''
     test_normal = torch.tensor(test_normal_signal, dtype=torch.float32).to(device)
@@ -132,7 +149,8 @@ def run_anomaly_quality_test(
     # print(f"train_normal.shape: {normal.shape}")
     # print(f"train_anomaly.shape: {anomaly.shape}")
     # breakpoint()
-    metrics = fit_classifier(model, train_loader, train_loader, lr)
+    metrics = fit_classifier(model, normal_train_loader, anomaly_train_loader, test_loader, lr)
+
 
     return metrics
 
