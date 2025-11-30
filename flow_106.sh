ae992f3719aa74abb9696a488f6eea2608a8de97
Haochen Zhang <zhc@Haochens-MacBook-Pro.local>
2025-12-01 00:17:16 +0800

    flow-evaluation

--- flow_106.sh
+++ flow_106.sh
@@ -24,9 +24,42 @@
 #  --gpu_id 2
 
 
-python FlowFinetunePipeline.py \
+#python FlowFinetunePipeline.py \
+#  --seq_len 800 \
+#  --feature_size 2 \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  \
+#  --max_anomaly_ratio 0.2 \
+#  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
+#  --normal_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/normal.jsonl" \
+#  --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
+#  --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
+#  \
+#  --lr 1e-5 \
+#  --batch_size 64 \
+#  --max_iters 1000000 \
+#  --grad_clip_norm 1.0 \
+#  \
+#  --wandb_project flow_mitdb106v_finetune \
+#  --wandb_run flow_mitdb106v_finetune \
+#  \
+#  --ckpt_dir "../TSA-ckpts/flow_mitdb106v_finetune_ckpt" \
+#  --pretrained_ckpt "../TSA-ckpts/flow_normal_pretrain_ckpt_mitdb106/2025-11-30-00:52:35/ckpt.pth" \
+#  --gpu_id 2
+
+
+
+
+
+python FlowEvaluate.py \
   --seq_len 800 \
   --feature_size 2 \
+  \
   --n_layer_enc 4 \
   --n_layer_dec 4 \
   --d_model 64 \
@@ -40,15 +73,16 @@ python FlowFinetunePipeline.py \
   --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
   --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
   \
-  --lr 1e-5 \
   --batch_size 64 \
-  --max_iters 1000000 \
-  --grad_clip_norm 1.0 \
   \
-  --wandb_project flow_mitdb106v_finetune \
-  --wandb_run flow_mitdb106v_finetune \
-  \
-  --ckpt_dir "../TSA-ckpts/flow_mitdb106v_finetune_ckpt" \
-  --pretrained_ckpt "../TSA-ckpts/flow_normal_pretrain_ckpt_mitdb106/2025-11-30-00:52:35/ckpt.pth" \
-  --gpu_id 2
+  --model_ckpt "../TSA-ckpts/flow_mitdb106v_finetune_ckpt/2025-11-30-03:01:14/ckpt.pth" \
+  --gpu_id 2 \
+  --num_samples 100 \
+  --generated_path "../samples_path/flow/mitdb106v-2025-11-30-03:01:14" \
+
+
+
+
+
+
 
