48e6ea522dde5bcda5b657df4d44b8d653ff4e3b
Haochen Zhang <hz112@rice.edu>
2025-12-02 04:15:57 +0800

    flow-tune

--- generation_models/FlowTS/interpretable_flow/transformer.py
+++ generation_models/FlowTS/interpretable_flow/transformer.py
@@ -200,8 +200,7 @@ class FullAttention(nn.Module):
 
 
         if int(os.environ.get('hucfg_attention_rope_use', '-1')) == 1:
-            print("using rope")
-            freqs_cis = self.freqs_cis.cuda()[0 : T]
+            freqs_cis = self.freqs_cis[0 : T].to(q.device)
             q, k = apply_rotary_emb(q.permute(0,2,1,3), k.permute(0,2,1,3), freqs_cis=freqs_cis)
             q, k = q.permute(0,2,1,3), k.permute(0,2,1,3)
 
