fa4c77eba7e00f701b2899654a1414274e81795e
Haochen Zhang <hz112@rice.edu>
2025-12-02 03:48:34 +0800

    cgats-evaluate

--- generation_models/FlowTS/interpretable_flow/transformer.py
+++ generation_models/FlowTS/interpretable_flow/transformer.py
@@ -575,6 +575,7 @@ class Transformer(nn.Module):
                 anomaly_label = self.anomaly_label_embedding(anomaly_label)
             elif isinstance(self.anomaly_label_embedding, nn.Conv1d):
                 model_dtype = next(self.parameters()).dtype
+                breakpoint()
                 anomaly_label = self.anomaly_label_embedding(anomaly_label.to(dtype=model_dtype))
             else:
                 raise NotImplementedError
