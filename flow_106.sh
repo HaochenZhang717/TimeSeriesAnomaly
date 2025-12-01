395f02710840f2f9c6c7eb5c58145ac20daab46f
Haochen Zhang <hz112@rice.edu>
2025-12-02 05:01:33 +0800

    flow-v2-eval

--- flow_106.sh
+++ flow_106.sh
@@ -138,34 +138,34 @@
 
 
 
-python FlowFinetunePipeline.py \
-  --seq_len 800 \
-  --feature_size 2 \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  --version 2 \
-  \
-  --max_anomaly_ratio 0.2 \
-  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
-  --normal_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/normal.jsonl" \
-  --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
-  --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
-  \
-  --lr 1e-5 \
-  --batch_size 64 \
-  --max_iters 1000000 \
-  --grad_clip_norm 1.0 \
-  \
-  --wandb_project flow_mitdb106v_finetune \
-  --wandb_run flow_mitdb106v_finetune_v2 \
-  \
-  --ckpt_dir "../TSA-ckpts/flow_mitdb106v_finetune_ckpt_v2" \
-  --pretrained_ckpt "../TSA-ckpts/flow_normal_pretrain_ckpt_mitdb106/2025-11-30-00:52:35/ckpt.pth" \
-  --gpu_id 2
+#python FlowFinetunePipeline.py \
+#  --seq_len 800 \
+#  --feature_size 2 \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  --version 2 \
+#  \
+#  --max_anomaly_ratio 0.2 \
+#  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
+#  --normal_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/normal.jsonl" \
+#  --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
+#  --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
+#  \
+#  --lr 1e-5 \
+#  --batch_size 64 \
+#  --max_iters 1000000 \
+#  --grad_clip_norm 1.0 \
+#  \
+#  --wandb_project flow_mitdb106v_finetune \
+#  --wandb_run flow_mitdb106v_finetune_v2 \
+#  \
+#  --ckpt_dir "../TSA-ckpts/flow_mitdb106v_finetune_ckpt_v2" \
+#  --pretrained_ckpt "../TSA-ckpts/flow_normal_pretrain_ckpt_mitdb106/2025-11-30-00:52:35/ckpt.pth" \
+#  --gpu_id 2
 
 
 
