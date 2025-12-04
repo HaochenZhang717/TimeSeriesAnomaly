de77b97f9b6b3d09e459a50c8edaad685fb92f51
Haochen Zhang <hz112@rice.edu>
2025-12-04 13:04:59 +0800

    fintune flow

--- flow_rope_anomaly_only.sh
+++ flow_rope_anomaly_only.sh
@@ -59,29 +59,29 @@
 #  --gpu_id 0
 
 
-export hucfg_attention_rope_use=1
-python FlowFinetuneEvaluateAnomaly.py \
-  --seq_len 800 \
-  --feature_size 2 \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  --version 2 \
-  \
-  --max_anomaly_ratio 0.2 \
-  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
-  --normal_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/normal.jsonl" \
-  --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
-  --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
-  \
-  --batch_size 64 \
-  \
-  --model_ckpt "../TSA-ckpts/flow_normal_finetune_ckpt_mitdb106_rope_v2_anomaly_only/2025-12-01-16:42:26/ckpt.pth" \
-  --gpu_id 7 \
-  \
-  --need_to_generate 1 \
-  --generated_path "../samples_path/flow/mitdb106v-finetuned-rope-v2-anomaly-only" \
+#export hucfg_attention_rope_use=1
+#python FlowFinetuneEvaluateAnomaly.py \
+#  --seq_len 800 \
+#  --feature_size 2 \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  --version 2 \
+#  \
+#  --max_anomaly_ratio 0.2 \
+#  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
+#  --normal_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/normal.jsonl" \
+#  --anomaly_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V.jsonl" \
+#  --anomaly_indices_paths_val "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/validation/V.jsonl" \
+#  \
+#  --batch_size 64 \
+#  \
+#  --model_ckpt "../TSA-ckpts/flow_normal_finetune_ckpt_mitdb106_rope_v2_anomaly_only/2025-12-01-16:42:26/ckpt.pth" \
+#  --gpu_id 7 \
+#  \
+#  --need_to_generate 1 \
+#  --generated_path "../samples_path/flow/mitdb106v-finetuned-rope-v2-anomaly-only" \
