798c8d451a0e4717280eb7b56db8922176f78e5b
Haochen Zhang <hz112@rice.edu>
2025-12-07 14:59:04 -0600

    imputation

--- generation_models/FlowTSGenTwoTogether/interpretable_flow/FMTS.py
+++ generation_models/FlowTSGenTwoTogether/interpretable_flow/FMTS.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from einops import reduce
-from Models.interpretable_diffusion.transformer import Transformer
+from .transformer import Transformer
 import os
 
 
