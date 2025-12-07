b86968adc80bca89a621d9297bc0bb11c143030e
Haochen Zhang <hz112@rice.edu>
2025-12-07 15:06:42 -0600

    imputation

--- generation_models/FlowTSGenTwoTogether/interpretable_flow/transformer.py
+++ generation_models/FlowTSGenTwoTogether/interpretable_flow/transformer.py
@@ -206,7 +206,7 @@ class FullAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
         if int(os.environ.get('hucfg_attention_rope_use', '-1')) == 1:
-            freqs_cis = self.freqs_cis.cuda()[0: T]
+            freqs_cis = self.freqs_cis[0: T].to(q.device)
             q, k = apply_rotary_emb(q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3), freqs_cis=freqs_cis)
             q, k = q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3)
 
@@ -279,7 +279,7 @@ class CrossAttention(nn.Module):
         k = k.view(B, T_E, self.n_head, C // self.n_head).transpose(1, 2)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
-        freqs_cis = self.freqs_cis.cuda()[0: T]
+        freqs_cis = self.freqs_cis[0: T].to(q.device)
         q, k = apply_rotary_emb(q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3), freqs_cis=freqs_cis)
         q, k = q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3)
 
