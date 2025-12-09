97270f122d334a7c99a7cad69e27f766b01d8f9b
Haochen Zhang <hz112@rice.edu>
2025-12-09 10:32:48 -0600

    evluate anomaly

--- cgats_finetune.sh
+++ cgats_finetune.sh
@@ -66,7 +66,7 @@ python CGATSFinetuneEvaluate.py \
   --hidden_layer_sizes "[50,100,200]" \
   --custom_seas "[[10,80],[20,40],[40,20],[80,10]]" \
   \
-  --max_anomaly_ratio 0.2 \
+  --max_anomaly_length 160 \
   --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
   --raw_data_paths_val "./dataset_utils/ECG_datasets/raw_data/106.npz" \
   --normal_indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
