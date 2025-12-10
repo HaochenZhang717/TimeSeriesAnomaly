94b230b46be63c3bf0166e74cf0a95d987137162
Haochen Zhang <hz112@rice.edu>
2025-12-10 01:54:38 -0600

    debug size eval

--- cuda0.sh
+++ cuda0.sh
@@ -234,47 +234,47 @@ python FlowTwoTogether.py \
 #  --gpu_id 0
 
 
-#export hucfg_t_sampling=logitnorm
-#python FlowTwoTogether.py \
-#  --what_to_do "conditional_sample_on_fake" \
-#  \
-#  --seq_len 1800 \
-#  --feature_size 1 \
-#  --one_channel 1 \
-#  \
-#  --n_layer_enc 4 \
-#  --n_layer_dec 4 \
-#  --d_model 64 \
-#  --n_heads 4 \
-#  \
-#  --dataset_name "ECG" \
-#  --max_anomaly_length 629 \
-#  --min_anomaly_length 74 \
-#  --raw_data_paths_train "none" \
-#  --raw_data_paths_val "none" \
-#  --indices_paths_train "none" \
-#  --indices_paths_val "none" \
-#  \
-#  --lr 5e-4 \
-#  --batch_size 64 \
-#  --max_epochs -1 \
-#  --grad_clip_norm -1.0 \
-#  --grad_accum_steps 1 \
-#  --early_stop "none" \
-#  --patience -1 \
-#  \
-#  --wandb_project "none" \
-#  --wandb_run "none" \
-#  \
-#  --ckpt_dir "none" \
-#  \
-#  --cond_eval_model_ckpt "../TSA-ckpts/flow_two_together_logit_normal/mitdb1800/conditional_ckpt/ema_ckpt.pth" \
-#  --generated_path "../samples_path/flow_two_together_logit_normal/mitdb1800" \
-#  --normal_data_path "../samples_path/flow_two_together_logit_normal/mitdb1800/generated_normal.pt" \
-#  \
-#  --uncond_eval_model_ckpt "none" \
-#  --uncond_num_samples -1 \
-#  \
-#  --eval_train_size -1 \
-#  \
-#  --gpu_id 0
+export hucfg_t_sampling=logitnorm
+python FlowTwoTogether.py \
+  --what_to_do "conditional_sample_on_fake" \
+  \
+  --seq_len 1800 \
+  --feature_size 1 \
+  --one_channel 1 \
+  \
+  --n_layer_enc 4 \
+  --n_layer_dec 4 \
+  --d_model 64 \
+  --n_heads 4 \
+  \
+  --dataset_name "ECG" \
+  --max_anomaly_length 629 \
+  --min_anomaly_length 74 \
+  --raw_data_paths_train "none" \
+  --raw_data_paths_val "none" \
+  --indices_paths_train "none" \
+  --indices_paths_val "none" \
+  \
+  --lr 5e-4 \
+  --batch_size 64 \
+  --max_epochs -1 \
+  --grad_clip_norm -1.0 \
+  --grad_accum_steps 1 \
+  --early_stop "none" \
+  --patience -1 \
+  \
+  --wandb_project "none" \
+  --wandb_run "none" \
+  \
+  --ckpt_dir "none" \
+  \
+  --cond_eval_model_ckpt "../TSA-ckpts/flow_two_together_logit_normal/mitdb1800/conditional_ckpt/ema_ckpt.pth" \
+  --generated_path "../samples_path/flow_two_together_logit_normal/mitdb1800" \
+  --normal_data_path "../samples_path/flow_two_together_logit_normal/mitdb1800/generated_normal.pt" \
+  \
+  --uncond_eval_model_ckpt "none" \
+  --uncond_num_samples -1 \
+  \
+  --eval_train_size -1 \
+  \
+  --gpu_id 0
