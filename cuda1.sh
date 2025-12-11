c9debfe26786d79dfb63c18e71441e20ca37e377
Haochen Zhang <hz112@rice.edu>
2025-12-10 21:32:29 -0600

    train_on_200npz

--- cuda1.sh
+++ cuda1.sh
@@ -1,31 +1,31 @@
-#python CGATSPretrainPipeline.py \
-#  --seq_len 1800 \
-#  --feature_size 1 \
-#  --one_channel 1 \
-#  \
-#  --latent_dim 64 \
-#  --trend_poly 3 \
-#  --kl_wt 1e-3 \
-#  --hidden_layer_sizes "[50,100,200]" \
-#  --custom_seas "[[10,80],[20,40],[40,20],[80,10]]" \
-#  \
-#  --dataset_name "ECG" \
-#  --max_anomaly_length 629 \
-#  --min_anomaly_length 74 \
-#  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-#  --indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
-#  \
-#  --lr 1e-5 \
-#  --batch_size 64 \
-#  --epochs 1000 \
-#  --grad_clip_norm 1.0 \
-#  --early_stop "true" \
-#  \
-#  --wandb_project cgats_pretrain \
-#  --wandb_run cgats_pretrain_mitdb1800 \
-#  \
-#  --ckpt_dir "../TSA-ckpts/cgats/mitdb1800/pretrain_ckpt" \
-#  --gpu_id 1
+python CGATSPretrainPipeline.py \
+  --seq_len 1800 \
+  --feature_size 1 \
+  --one_channel 1 \
+  \
+  --latent_dim 64 \
+  --trend_poly 3 \
+  --kl_wt 1e-3 \
+  --hidden_layer_sizes "[50,100,200]" \
+  --custom_seas "[[10,80],[20,40],[40,20],[80,10]]" \
+  \
+  --dataset_name "ECG" \
+  --max_anomaly_length 629 \
+  --min_anomaly_length 74 \
+  --raw_data_paths_train "./dataset_utils/ECG_datasets/raw_data/200.npz" \
+  --indices_paths_train "./dataset_utils/ECG_datasets/indices/slide_windows_200npz/train/normal.jsonl" \
+  \
+  --lr 1e-5 \
+  --batch_size 64 \
+  --epochs 1000 \
+  --grad_clip_norm 1.0 \
+  --early_stop "true" \
+  \
+  --wandb_project cgats_pretrain \
+  --wandb_run cgats_pretrain_mitdb1800 \
+  \
+  --ckpt_dir "../TSA-ckpts/cgats/mitdb1800/pretrain_ckpt" \
+  --gpu_id 1
 
 
 
