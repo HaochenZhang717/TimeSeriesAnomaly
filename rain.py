18a13664158f81d2d36f9e85b36bed952815939f
Haochen Zhang <hz112@rice.edu>
2025-12-14 15:00:35 -0600

    rain

--- rain.py
+++ rain.py
@@ -27,7 +27,7 @@ def get_args():
     parser.add_argument(
         "--what_to_do", type=str, required=True,
         choices=[
-            "autoencoder_train"
+            "autoencoder_train",
             "flow_training",],
         help="what to do"
     )
