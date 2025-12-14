65afc62a2a9946c32e3d8905ea628b1cdce4bbdd
Haochen Zhang <hz112@rice.edu>
2025-12-13 18:13:04 -0600

    eval

--- dataset_utils/ECG_datasets/ecg_datasets.py
+++ dataset_utils/ECG_datasets/ecg_datasets.py
@@ -59,7 +59,8 @@ class ECGDataset(Dataset):
 
     def __getitem__(self, index):
         signal = self.slide_windows[index]
-        anomaly_label = self.anomaly_labels[index] > 0
+        anomaly_label = self.anomaly_labels[index]
+        breakpoint()
 
         random_anomaly_length = np.random.randint(self.min_anomaly_length, self.max_anomaly_length)
         anomaly_start = np.random.randint(0, self.max_anomaly_length - random_anomaly_length)
