3ea5a53fb96bef4b9e6af4f9cb1a1bfbb841587b
Haochen Zhang <hz112@rice.edu>
2025-12-15 17:20:12 -0600

    prototype flow

--- prototype_flow_cuda0.sh
+++ prototype_flow_cuda0.sh
@@ -1,7 +1,43 @@
 SEQLEN=100
+#export hucfg_t_sampling=logitnorm
+#python PrototypeFlow.py \
+#  --what_to_do "no_context_train" \
+#  \
+#  --seq_len ${SEQLEN} \
+#  --feature_size 1 \
+#  --one_channel 1 \
+#  \
+#  --n_layer_enc 4 \
+#  --n_layer_dec 4 \
+#  --d_model 64 \
+#  --n_heads 4 \
+#  --num_prototypes 8 \
+#  \
+#  --raw_data_path_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#  --indices_path_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
+#  \
+#  --lr 1e-4 \
+#  --batch_size 64 \
+#  --max_epochs 1000 \
+#  --grad_clip_norm 1.0 \
+#  --grad_accum_steps 1 \
+#  --early_stop "true" \
+#  --patience 50 \
+#  \
+#  --wandb_project "Prototype Flow" \
+#  --wandb_run "106npz-no-context-normal" \
+#  \
+#  --ckpt_dir "../TSA-ckpts/PrototypeFlow/mitdb1800_106/normal_pretrain_len${SEQLEN}_ckpt" \
+#  \
+#  --generated_dir "none" \
+#  \
+#  --gpu_id 0
+
+
+
 export hucfg_t_sampling=logitnorm
 python PrototypeFlow.py \
-  --what_to_do "no_context_train" \
+  --what_to_do "normal_sample" \
   \
   --seq_len ${SEQLEN} \
   --feature_size 1 \
@@ -13,44 +49,8 @@ python PrototypeFlow.py \
   --n_heads 4 \
   --num_prototypes 8 \
   \
-  --raw_data_path_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --indices_path_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/normal.jsonl" \
-  \
-  --lr 1e-4 \
-  --batch_size 64 \
-  --max_epochs 1000 \
-  --grad_clip_norm 1.0 \
-  --grad_accum_steps 1 \
-  --early_stop "true" \
-  --patience 50 \
-  \
-  --wandb_project "Prototype Flow" \
-  --wandb_run "106npz-no-context-normal" \
-  \
-  --ckpt_dir "../TSA-ckpts/PrototypeFlow/mitdb1800_106/normal_pretrain_len${SEQLEN}_ckpt" \
-  \
-  --generated_dir "none" \
-  \
-  --gpu_id 0
-
-
-
-export hucfg_t_sampling=logitnorm
-python PrototypeFlow.py \
-  --what_to_do "no_context_sample" \
-  \
-  --seq_len ${SEQLEN} \
-  --feature_size 1 \
-  --one_channel 1 \
-  \
-  --n_layer_enc 4 \
-  --n_layer_dec 4 \
-  --d_model 64 \
-  --n_heads 4 \
-  --num_prototypes 8 \
-  \
-  --raw_data_path_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-  --indices_path_train "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/anomaly_segments_with_prototype.jsonl" \
+  --raw_data_path_train "none" \
+  --indices_path_train "none" \
   \
   --lr 1e-4 \
   --batch_size 64 \
