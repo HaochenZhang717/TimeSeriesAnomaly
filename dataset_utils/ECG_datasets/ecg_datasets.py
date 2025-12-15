06ab316876ce2a7c76a51a478926e935fb2fe26b
Haochen Zhang <hz112@rice.edu>
2025-12-15 10:42:41 -0600

    prototype flow

--- dataset_utils/ECG_datasets/ecg_datasets.py
+++ dataset_utils/ECG_datasets/ecg_datasets.py
@@ -202,7 +202,6 @@ class NoContextECGDataset(Dataset):
         else:
             signal = torch.from_numpy(self.normed_signal[start:end])
         anomaly_label = self.anomaly_label[start:end]
-        breakpoint()
         assert min(anomaly_label) == 1
         assert max(anomaly_label) == 1
 
