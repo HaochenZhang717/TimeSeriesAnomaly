1a820a593abc4fc29bb7c10971a95be3384a88cb
Haochen Zhang <hz112@rice.edu>
2025-12-15 14:52:30 -0600

    prototype flow

--- generation_models/PrototypeFlow/interpretable_flow/transformer.py
+++ generation_models/PrototypeFlow/interpretable_flow/transformer.py
@@ -279,6 +279,7 @@ class CrossAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
         freqs_cis = self.freqs_cis[0: T].to(q.device)
+        breakpoint()
         q, k = apply_rotary_emb(q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3), freqs_cis=freqs_cis)
         q, k = q.permute(0, 2, 1, 3), k.permute(0, 2, 1, 3)
 
