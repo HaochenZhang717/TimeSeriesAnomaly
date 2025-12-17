9747501d577dfa94a39a8dd52885400e9fe2bf80
Haochen Zhang <hz112@rice.edu>
2025-12-17 12:25:49 -0600

    prototype flow

--- vqvae_train/mini_runnable.py
+++ vqvae_train/mini_runnable.py
@@ -534,6 +534,7 @@ def train_vqvae(cfg: TrainConfig):
             epoch_ids.append(ids.detach())
 
         # -------- codebook stats --------
+        breakpoint()
         stats = _compute_codebook_stats(epoch_ids, cfg.num_codes)
 
         # -------- logging --------
