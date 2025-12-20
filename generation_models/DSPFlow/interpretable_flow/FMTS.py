a1c54b67fd955befb3b207263c22e60bd9804bc3
Haochen Zhang <hz112@rice.edu>
2025-12-20 11:15:29 -0600

    dps

--- generation_models/DSPFlow/interpretable_flow/FMTS.py
+++ generation_models/DSPFlow/interpretable_flow/FMTS.py
@@ -47,8 +47,8 @@ class DSPFlow(nn.Module):
             seq_len=800
         )
         # when debug i comment this out
-        # pretrained_vqvae_ckpt = torch.load(vqvae_ckpt)
-        # self.vqvae.load_state_dict(pretrained_vqvae_ckpt["model_state"])
+        pretrained_vqvae_ckpt = torch.load(vqvae_ckpt)
+        self.vqvae.load_state_dict(pretrained_vqvae_ckpt["model_state"])
 
         for param in self.vqvae.parameters():
             param.requires_grad = False
