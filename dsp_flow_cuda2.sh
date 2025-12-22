16b943d887967ea493f9d2ad9cf256b0862b6b8f
Haochen Zhang <hz112@rice.edu>
2025-12-21 21:34:36 -0600

    dsp_flow

--- dsp_flow_cuda2.sh
+++ dsp_flow_cuda2.sh
@@ -79,42 +79,42 @@ for LR in "${LRS[@]}"; do
     --gpu_id 2
 
 
-
-  python dsp_flow.py \
-    --what_to_do "anomaly_evaluate" \
-    \
-    --seq_len 1000 \
-    --feature_size 1 \
-    --one_channel 1 \
-    \
-    --n_layer_enc 4 \
-    --n_layer_dec 4 \
-    --d_model 64 \
-    --n_heads 4 \
-    \
-    --raw_data_path_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
-    --indices_path_train "" \
-    --indices_path_test "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V_test.jsonl" \
-    --indices_path_anomaly_for_sample "" \
-    --min_infill_length 180 \
-    --max_infill_length 800 \
-    \
-    --lr 1e-4 \
-    --batch_size 64 \
-    --max_epochs 2000 \
-    --grad_clip_norm 1.0 \
-    --grad_accum_steps 1 \
-    --early_stop "true" \
-    --patience 50 \
-    \
-    --wandb_project "none" \
-    --wandb_run "none" \
-    \
-    --ckpt_dir "" \
-    --pretrained_ckpt "none" \
-    --vqvae_ckpt "" \
-    \
-    --generated_path "../TSA-ckpts/dsp_flow/106npz/impute_finetune_ckpt_lr${LR}/posterior_impute_samples.pth" \
-    \
-    --gpu_id 2
+#
+#  python dsp_flow.py \
+#    --what_to_do "anomaly_evaluate" \
+#    \
+#    --seq_len 1000 \
+#    --feature_size 1 \
+#    --one_channel 1 \
+#    \
+#    --n_layer_enc 4 \
+#    --n_layer_dec 4 \
+#    --d_model 64 \
+#    --n_heads 4 \
+#    \
+#    --raw_data_path_train "./dataset_utils/ECG_datasets/raw_data/106.npz" \
+#    --indices_path_train "" \
+#    --indices_path_test "./dataset_utils/ECG_datasets/indices/slide_windows_106npz/train/V_test.jsonl" \
+#    --indices_path_anomaly_for_sample "" \
+#    --min_infill_length 180 \
+#    --max_infill_length 800 \
+#    \
+#    --lr 1e-4 \
+#    --batch_size 64 \
+#    --max_epochs 2000 \
+#    --grad_clip_norm 1.0 \
+#    --grad_accum_steps 1 \
+#    --early_stop "true" \
+#    --patience 50 \
+#    \
+#    --wandb_project "none" \
+#    --wandb_run "none" \
+#    \
+#    --ckpt_dir "" \
+#    --pretrained_ckpt "none" \
+#    --vqvae_ckpt "" \
+#    \
+#    --generated_path "../TSA-ckpts/dsp_flow/106npz/impute_finetune_ckpt_lr${LR}/posterior_impute_samples.pth" \
+#    \
+#    --gpu_id 2
 done
